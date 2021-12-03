1c8c571e0e065b89c4e6a7ac37619f138b9338fe
Jason Goemaat <jason.goemaat@gmail.com>
2021-12-03 15:07:28 -0600

    Initial commit - working well with wide beam

---
+++ Scripts/AutomatonDetectorBeam/DetectorBeam.cs
@@ -0,0 +1,212 @@
+﻿using AtomicTorch.CBND.CoreMod.Characters;
+using AtomicTorch.CBND.CoreMod.Helpers.Client;
+using AtomicTorch.CBND.CoreMod.Systems.Weapons;
+using AtomicTorch.CBND.GameApi.Data.Characters;
+using AtomicTorch.CBND.GameApi.Data.Physics;
+using AtomicTorch.CBND.GameApi.Resources;
+using AtomicTorch.CBND.GameApi.Scripting;
+using AtomicTorch.CBND.GameApi.Scripting.ClientComponents;
+using AtomicTorch.CBND.GameApi.ServicesClient.Components;
+using AtomicTorch.CBND.GameApi.ServicesClient.Rendering;
+using AtomicTorch.GameEngine.Common.Primitives;
+using CryoFall.Automaton.Features;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace AutomatonDetectorBeam.Scripts.AutomatonDetectorBeam
+{
+    public class DetectorBeam : ClientComponent
+    {
+        private Color DefaultColor = Color.FromArgb(255, 255, 0, 0);
+
+        private ComponentBeam componentBeam;
+
+        public static Vector2D? TargetPosition = null;
+
+        private static DetectorBeam Instance;
+
+        public static DetectorBeam GetInstance()
+        {
+            if (Instance == null)
+            {
+                var character = ClientCurrentCharacterHelper.Character;
+                if (character != null)
+                {
+
+                    var sceneObject = character.ClientSceneObject;
+                    Instance = sceneObject.AddComponent<DetectorBeam>();
+                }
+            }
+
+            return Instance;
+        }
+
+        public DetectorBeam() : base(isLateUpdateEnabled: true)
+        {
+            // Api.Logger.Important($"TestComponent: Component being created()");
+        }
+
+        protected override void OnDisable()
+        {
+            this.componentBeam.Destroy();
+            this.componentBeam = null;
+        }
+
+        protected override void OnEnable()
+        {
+            this.componentBeam = this.SceneObject.AddComponent<ComponentBeam>(isEnabled: false);
+        }
+
+        public override void LateUpdate(double deltaTime)
+        {
+            var player = ClientCurrentCharacterHelper.Character;
+            if (player == null)
+            {
+                return;
+            }
+
+            var characterPublicState = player.GetPublicState<ICharacterPublicState>();
+            if (characterPublicState.IsDead || characterPublicState is PlayerCharacterPublicState { IsOnline: false })
+            {
+                this.componentBeam.IsEnabled = false;
+                return;
+            }
+
+            var clientState = player.GetClientState<BaseCharacterClientState>();
+            if (clientState.SkeletonRenderer is null || !clientState.SkeletonRenderer.IsReady)
+            {
+                this.componentBeam.IsEnabled = false;
+                return;
+            }
+
+            if (TargetPosition == null)
+            {
+                // no target, disable beam
+                this.componentBeam.IsEnabled = false;
+                return;
+            }
+
+            CastLine(player,
+                     customTargetPosition: TargetPosition,
+                     rangeMax: 100,
+                     toPosition: out var toPosition);
+
+            var sourcePosition = Vector2D.Zero;
+            var beamEndPosition = toPosition;
+
+            // fade-out if too close to prevent visual glitches
+            var distance = (beamEndPosition - sourcePosition).Length;
+            double beamOpacity = 1.0;
+
+            var color = DefaultColor;
+            sourcePosition = player.Position;
+
+            this.componentBeam.IsEnabled = true;
+            this.componentBeam.Refresh(
+                sourcePosition: sourcePosition,
+                sourcePositionOffset: 0.1,
+                targetPosition: beamEndPosition,
+                beamWidth: 1.0,
+                beamColor: color,
+                spotColor: color,
+                beamOpacity: beamOpacity,
+                // determine whether the beam should end with a bright spot (when pointing on something)
+                hasTarget: false);
+        }
+
+        private static void CastLine(
+                    ICharacter character,
+                    Vector2D? customTargetPosition,
+                    double rangeMax,
+                    out Vector2D toPosition)
+        {
+            var clientState = character.GetClientState<BaseCharacterClientState>();
+            var characterRotationAngleRad = clientState.LastInterpolatedRotationAngleRad.HasValue
+                                                ? clientState.LastInterpolatedRotationAngleRad.Value
+                                                : ((IProtoCharacterCore)character.ProtoCharacter)
+                                                .SharedGetRotationAngleRad(character);
+
+            WeaponSystem.SharedCastLine(character,
+                                        isMeleeWeapon: false,
+                                        rangeMax,
+                                        characterRotationAngleRad,
+                                        customTargetPosition: customTargetPosition,
+                                        fireSpreadAngleOffsetDeg: 0,
+                                        collisionGroup: null,
+                                        toPosition: out toPosition,
+                                        tempLineTestResults: out var tempLineTestResults,
+                                        sendDebugEvent: false);
+        }
+
+        private class ComponentBeam : ClientComponent
+        {
+            private const double SpotScale = 4.0;
+
+            private static readonly EffectResource BeamEffectResource
+                = new("AdditiveColorEffect");
+
+            private static readonly TextureResource TextureResourceBeam
+                = new("FX/WeaponTraces/BeamLaser.png");
+
+            private static readonly TextureResource TextureResourceSpot
+                = new("FX/Special/LaserSightSpot.png");
+
+            private readonly RenderingMaterial renderingMaterial
+                = RenderingMaterial.Create(BeamEffectResource);
+
+            private IComponentSpriteRenderer spriteRendererLine;
+
+            public void Refresh(
+                Vector2D sourcePosition,
+                double sourcePositionOffset,
+                Vector2D targetPosition,
+                double beamWidth,
+                Color beamColor,
+                Color spotColor,
+                double beamOpacity,
+                bool hasTarget)
+            {
+                beamColor = Color.FromArgb((byte)(beamOpacity * beamColor.A),
+                                           beamColor.R,
+                                           beamColor.G,
+                                           beamColor.B);
+
+                this.renderingMaterial.EffectParameters.Set("ColorAdditive", beamColor);
+                this.spriteRendererLine.Color = beamColor;
+
+                var deltaPos = targetPosition - sourcePosition;
+
+                ComponentWeaponTrace.CalculateAngleAndDirection(deltaPos,
+                                                                out var angleRad,
+                                                                out var normalizedRay);
+                sourcePosition += normalizedRay * sourcePositionOffset;
+                deltaPos = targetPosition - sourcePosition;
+
+                var sceneObjectPosition = this.spriteRendererLine.SceneObject.Position;
+                this.spriteRendererLine.PositionOffset = sourcePosition - sceneObjectPosition;
+                this.spriteRendererLine.RotationAngleRad = (float)angleRad;
+                this.spriteRendererLine.Size = (ScriptingConstants.TileSizeVirtualPixels * deltaPos.Length,
+                                                ScriptingConstants.TileSizeVirtualPixels * beamWidth);
+            }
+
+            protected override void OnDisable()
+            {
+                this.spriteRendererLine.Destroy();
+            }
+
+            protected override void OnEnable()
+            {
+                var sceneObject = this.SceneObject;
+                this.spriteRendererLine = Api.Client.Rendering.CreateSpriteRenderer(
+                    sceneObject,
+                    textureResource: TextureResourceBeam,
+                    spritePivotPoint: (0, 0.5),
+                    drawOrder: DrawOrder.Light);
+                this.spriteRendererLine.RenderingMaterial = this.renderingMaterial;
+                this.spriteRendererLine.BlendMode = BlendMode.AdditivePremultiplied;
+            }
+        }
+    }
+}
---
+++ Scripts/AutomatonDetectorBeam/FeatureDetectorBeam.cs
@@ -0,0 +1,148 @@
+﻿using AtomicTorch.CBND.CoreMod.Helpers.Client;
+using AtomicTorch.CBND.CoreMod.StaticObjects.Loot;
+using AtomicTorch.CBND.CoreMod.StaticObjects.Minerals;
+using AtomicTorch.CBND.CoreMod.StaticObjects.Misc.Events;
+using AtomicTorch.CBND.CoreMod.StaticObjects.Vegetation.SmallGatherables;
+using AtomicTorch.CBND.GameApi.Data;
+using AtomicTorch.CBND.GameApi.Data.World;
+using AtomicTorch.CBND.GameApi.Scripting;
+using AtomicTorch.CBND.GameApi.Scripting.ClientComponents;
+using AtomicTorch.GameEngine.Common.Primitives;
+using AutomatonDetectorBeam.Scripts.AutomatonDetectorBeam;
+using CryoFall.Automaton.ClientSettings;
+using CryoFall.Automaton.ClientSettings.Options;
+using CryoFall.Automaton.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomatonDetectorBeam.Scripts
+{
+    public class FeatureDetectorBeam : ProtoFeature<FeatureDetectorBeam>
+    {
+        private FeatureDetectorBeam() { }
+
+        public override string Name => "Detector Beam";
+
+        public override string Description => "Point a beam from player's feet to the nearest object of the selected types";
+
+        public List<IProtoEntity> ObjectList { get; set; }
+
+        public List<IProtoEntity> EnabledObjectList { get; set; }
+
+        protected override void PrepareFeature(List<IProtoEntity> entityList, List<IProtoEntity> requiredItemList)
+        {
+            ObjectList = new List<IProtoEntity>();
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectMineralPragmiumSource>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectMineralPragmiumNode>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSpaceDebris>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectMeteorite>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectMineralCoal>());
+
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectLootCrateFood>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectLootCrateHightech>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectLootCrateIndustrial>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectLootCrateMedical>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectLootCrateMilitary>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectLootCrateSupply>());
+
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSmallHerbPurple>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSmallHerbRed>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSmallHerbGreen>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSmallHerbBlue>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSmallMushroomPennyBun>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSmallMushroomPink>());
+            ObjectList.AddRange(Api.FindProtoEntities<ObjectSmallMushroomRust>());
+        }
+
+
+        public override void PrepareOptions(SettingsFeature settingsFeature)
+        {
+            AddOptionIsEnabled(settingsFeature);
+            Options.Add(new OptionSeparator());
+            Options.Add(new OptionEntityList(
+                parentSettings: settingsFeature,
+                id: "DetectorBeamObjectList",
+                entityList: ObjectList.OrderBy(entity => entity.Id),
+                defaultEnabledList: new List<string>(),
+                onEnabledListChanged: enabledList => EnabledObjectList = enabledList));
+        }
+
+        private bool IsValidObject(IStaticWorldObject staticWorldObject)
+        {
+            return EnabledObjectList.Contains(staticWorldObject.ProtoStaticWorldObject);
+        }
+
+        /// <summary>
+        /// Called by client component on specific time interval.
+        /// </summary>
+        public override void Execute()
+        {
+            if (!(IsEnabled && CheckPrecondition()))
+            {
+                return;
+            }
+
+            var component = DetectorBeam.GetInstance();
+            if (component == null)
+            {
+                return;
+            }
+
+            var target =
+                Api.Client.World.GetStaticWorldObjectsOfProto<IProtoStaticWorldObject>()
+                    .Where(IsValidObject)
+                    .OrderBy(o => CurrentCharacter.Position.DistanceTo(o.TilePosition.ToVector2D()))
+                    .FirstOrDefault();
+
+            if (target == null)
+            {
+                DetectorBeam.TargetPosition = null;
+                return;
+            }
+
+            var player = ClientCurrentCharacterHelper.Character;
+
+            // some magic to better center the location, default location for space debris is like 3 tiles sw
+            var sum = target.OccupiedTilePositions.Aggregate(Vector2D.Zero, (a, b) => a + b.ToVector2D());
+            var targetPosition = new Vector2D(sum.X / target.OccupiedTilePositions.Count(), sum.Y / target.OccupiedTilePositions.Count());
+            targetPosition += new Vector2D(0.5, 0.5); // center of a tile
+
+            DetectorBeam.TargetPosition = targetPosition;
+        }
+
+
+        /// <summary>
+        /// Called by client component every tick.
+        /// </summary>
+        public override void Update(double deltaTime)
+        {
+        }
+
+
+        /// <summary>
+        /// Stop everything.
+        /// </summary>
+        public override void Stop()
+        {
+            DetectorBeam.TargetPosition = null;
+        }
+
+        /// <summary>
+        /// Setup any of subscriptions
+        /// </summary>
+        public override void SetupSubscriptions(ClientComponent parentComponent)
+        {
+            base.SetupSubscriptions(parentComponent);
+        }
+
+        /// <summary>
+        /// Init on component enabled.
+        /// </summary>
+        public override void Start(ClientComponent parentComponent)
+        {
+            base.Start(parentComponent);
+        }
+    }
+}
